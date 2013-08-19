bdb0b9e6740b9f673322e7884fa372e415a1c0ab
Mike Infanger <mikfanger@gmail.com>
2013-08-18 21:15:06 -0500

    Updated: -Android classpath -ExplainationActivity, reverted to blank sheet

--- Roamer/src/com/example/roamer/LoginActivity.java
+++ Roamer/src/com/example/roamer/LoginActivity.java
@@ -57,7 +57,7 @@ public class LoginActivity extends Activity {
 
 		// Set up the login form.
 		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
-		mEmailView = (EditText) findViewById(R.id.email);
+		mEmailView = (EditText) findViewById(R.id.login);
 		mEmailView.setText(mEmail);
 
 		mPasswordView = (EditText) findViewById(R.id.password);
