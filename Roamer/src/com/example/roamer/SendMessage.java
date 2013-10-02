9aad93675e0247fb666b950b086e1e87cc63c7bc
Johann SB <Johann SB@JohannSB-PC>
2013-10-01 23:50:21 -0500

    Added new backgrounds, updated the home screen.

--- Roamer/src/com/example/roamer/SendMessage.java
+++ Roamer/src/com/example/roamer/SendMessage.java
@@ -1,6 +1,5 @@
 package com.example.roamer;
 
-import com.example.roamer.location.MyLocation;
 import com.example.roamer.location.MyLocation.LocationResult;
 
 import android.location.Location;
@@ -15,7 +14,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class SendMessage extends Activity {
